aa8aa3c6567303c4c55003f578eea88fbf7e38ee
Fandoamr <mooraalees157@gmail.com>
2021-06-30 19:16:18 +0100

    Code reestructured

---
+++ MarvelApp/CollectionCells/SeriesCell.swift
@@ -0,0 +1,30 @@
+//
+//  SeriesCell.swift
+//  MarvelApp
+//
+//  Created by admin on 6/29/21.
+//
+
+import UIKit
+
+class SeriesCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageSeries: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    func setSeries(data: SeriesResult){
+        
+        guard let securePath = data.thumbnail?.path else { return }
+        guard let secureExtension = data.thumbnail?.imageExtension else { return }
+        
+        let secureURL = "\(securePath.replacingOccurrences(of: "https", with: "https")).\(secureExtension)"
+        let url = URL(string: secureURL)
+        
+        //print("\(url)")
+        imageSeries.kf.setImage(with: url)
+    }
+    
+}
